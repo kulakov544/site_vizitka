35e5cb854520ae719d02cccd17bc8f64256d1b2f
akulakov <akulakov544@gmail.com>
2024-08-07 16:29:20 +0300

    инфр

--- page/show_project.py
+++ page/show_project.py
@@ -64,7 +64,7 @@ def show_project():
 2. Создайте виртуальное окружение и активируйте его:
    ```bash
    python -m venv venv
-   source venv/bin/activate  # Для Windows используйте `venv\Scripts\activate`
+   source venv/bin/activate  # Для Windows используйте `venv\\Scripts\\activate`
    ```
 3. Установите необходимые зависимости:
    ```bash
@@ -83,9 +83,261 @@ def show_project():
 
     st.write("---")
 
-    with st.expander("Проект: В процессе"):
+    with st.expander("Проект: Анализ вакансий на hh.ru. ETL"):
         st.markdown("""
-        - Описание: Будет добавлено.
-        - Технологии: Python
-        - [Ссылка на GitHub](https://github.com/kulakov544/)
-        """)
+        ## Анализ вакансий на hh.ru        
+        Все три репозитория по этому проекту размещены на GitHab:
+        1. pars_hh_ETL https://github.com/kulakov544/pars_hh_ETL.git
+        2. pars_hh_DWH https://github.com/kulakov544/pars_hh_DWH.git
+        3. pars_hh_Report https://github.com/kulakov544/pars_hh_report.git
+        
+        ### Бизнес требование
+        Нужно узнать какие навыки требуются на рынке вакансий.
+        
+        ### Техническое задание
+        Получить аналитику по требованиям к соискателям вакансий на сайте hh.ru по IT направлению.
+        
+        ### Решение
+        1. Построить базу DWH на PostgreSQL.
+        2. Получить данные о вакансиях с сайта hh.ru
+        3. Провести первичную обработка данных для загрузки в базу.
+        4. Загрузить данные в Stage слой DWH.
+        5. Трансформировать данные для укладки на долговременное хранение.
+        6. Заполнить справочники и таблицу вакансий в Core слое DWH, с сохранением обновленных записей в таблицу истории.
+        7. Оркестрация процесса через Prefect.
+        8. Сделать View и материализацию таблиц в DataMart слое DWH
+        9. По данным из DataMart построить дашборды в Yandex Data Lens и Power BI
+        
+        
+        ### План реализации
+        Схема работы программы:
+        <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_ETL/master/flow_pars_hh_dir/file/DWH.png' style='width: 100%; height: auto;'><br>
+        Подробную схему DWH можно посмотреть в соответствующем проекте:  
+        https://github.com/kulakov544/pars_hh_DWH.git
+        
+        ### Работа программы:
+        1. Создание списка наборов параметров search_params_list.
+        2. Создание сета для отбора уникальных id вакансий uniq_id
+        3. Разбивка наборов параметров на пакеты по 100 штук.
+        4. Формирование списка id вакансий.(vacancies_id_df)
+           1. Получает 100 наборов параметров
+           2. В цикле запрашивает данные по каждому набору параметров
+           3. Получает вакансии по этому набору и проверяет есть ли id вакансий
+           в uniq_id. Если нет, то добавляет его туда и в массив vacancies_data.
+           4. Когда пройдет по всем вакансиям из этого набора параметров добавляет их
+           в all_vacancies_id_df и берет следующий набор параметров.
+           5. Полученный датафрейм возвращается в vacancies_id_df
+        5.  По полученному списку id начинает собирать информацию о вакансиях.
+              1. Запросы делаются отдельно для каждого id.
+              2. В all_vacancies_data собирается основная информация о вакансии. 
+              В all_vacancies_skill собираются скилы необходимые для этой вакансии.
+            3. Тут же проходит первичная обработка. Проверки на наличие данных, добавление 
+            хеша вакансии, изменение форматов дат...
+            4. Полученные датафреймы возвращаются в vacancies_data_df и vacancies_skill_df
+        6. Если данные были собраны они загружаются в соответствующие таблицы с помощью put_data.
+        Это происходит с помощью replace по этому при каждой загрузке таблицы очищаются.
+        7. Когда данные загружены запускаются функции переноса данных из stage в core c
+        помощью функции update_core
+           1. В таблицу stage_pars_hh добавляется столбец статуса. Он зависит от того есть 
+           эта вакансия в базе и совпадают ли хеши с теми которые уже есть.
+           2. Заполняются справочники включая скилы, если для них есть новые данные.
+           3. Переносятся вакансии которых ещё не было в базе.
+           4. Вакансии которые обновились, переносятся в fact_history и удаляются из основной таблицы.
+           5. Вместо них загружаются новые записи.
+           6. Обновляется таблица мапинга скилов и вакансий.
+        8. Всё это является потоком Prefect и в основных функциях проставлены таски.
+        9. Отдельным потоком является получение курсов валют.
+        10. В слое Data Mart созданы представления, которые строят таблицы с нужными данными.
+        По этим таблицам построен дашборд в PowerBI. Так же эти таблицы можно просто просмотреть любой 
+        программой для работы с базами данных.
+        
+        
+        ## Используемые технологии
+        
+        ![Python](https://img.shields.io/badge/Python-3.12+-blue?logo=python)
+        ![Pandas](https://img.shields.io/badge/Pandas-1.3.3+-yellow?logo=pandas)
+        ![SQLAlchemy](https://img.shields.io/badge/SQLAlchemy-1.4.22+-orange?logo=python)
+        ![Prefect](https://img.shields.io/badge/prefect-2.19.8+-green?logo=prefect)
+        ![Requests](https://img.shields.io/badge/Requests-2.32.3+-green?logo=python)
+        ![numpy](https://img.shields.io/badge/numpy-2.0.1+-green?logo=python)
+        
+        ## Установка
+        
+        1. Клонируйте репозиторий:
+           ```bash
+           git clone https://github.com/kulakov544/prefect_dev.git 
+           ```
+        2. Создайте виртуальное окружение и активируйте его:
+           ```bash
+           python -m venv venv
+           source venv/bin/activate  # Для Windows используйте `venv\\Scripts\\activate`
+           ```
+        3. Установите необходимые зависимости:
+           ```bash
+           pip install -r requirements.txt
+           ```
+        4. В папке проекта создайте файл .env с данными для подключения к вашей базе данных.
+        access_token можно получить с помощью скриптов Authorization.py и authorization_code.py
+        если у вас есть коды для его получения, которые можно запросить на https://dev.hh.ru/
+           ```bash
+           conn_string = 'postgresql+psycopg2://user:password@localhost:port/mydatabase'
+           access_token = "APP*"
+           ```
+        5. Авторизуйтесь на сервере prefect
+        ```
+        prefect cloud login
+        ```
+        5. Запустите программу.
+           ```bash
+           python main.py
+           ```
+
+        """, unsafe_allow_html=True)
+
+    st.write("---")
+
+    with st.expander("Проект: Анализ вакансий на hh.ru. DWH"):
+        st.markdown("""
+            ## Анализ вакансий на hh.ru
+
+            Данный репозиторий является частью проекта по анализу вакансий с hh.ru.  
+            Здесь будут размещены данные о ETL.
+            Всего таких репозиториев три:
+            1. pars_hh_ETL https://github.com/kulakov544/pars_hh_ETL.git
+            2. pars_hh_DWH https://github.com/kulakov544/pars_hh_DWH.git
+            3. pars_hh_Report https://github.com/kulakov544/pars_hh_report.git
+            
+            ### Бизнес требование
+            Нужно узнать какие навыки требуются на рынке вакансий.
+            
+            ### Техническое задание
+            Получить аналитику по требованиям к соискателям вакансий на сайте hh.ru по IT направлению.
+            
+            ### Решение
+            1. Построить базу DWH на PostgreSQL.
+            2. Получить данные о вакансиях с сайта hh.ru
+            3. Провести первичную обработка данных для загрузки в базу.
+            4. Загрузить данные в Stage слой DWH.
+            5. Трансформировать данные для укладки на долговременное хранение.
+            6. Заполнить справочники и таблицу вакансий в Core слое DWH, с сохранением обновленных записей в таблицу истории.
+            7. Оркестрация процесса через Prefect.
+            8. Сделать View и материализацию таблиц в DataMart слое DWH
+            9. По данным из DataMart построить дашборды в Yandex Data Lens и Power BI  
+
+            <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_DWH/master/file/DWH.png' style='width: 100%; height: auto;'><br>
+            
+            ### План DWH
+            Таблицы слоя Stage:
+            Слой Stage содержит две таблицы. stage_pars_hh и stage_pars_hh_skill.  
+            stage_pars_hh основная таблица с данными о вакансии. В неё попадает информация из 
+            ответа Api HH после небольшой обработки.
+            stage_pars_hh_skill это навыки указанные в вакансии. Они специально вынесены в отдельную таблицу.
+            Навыков в одной вакансии может быть много и их надо, либо хранить в неудобном формате, либо загружать 
+            отдельной таблицей и сразу отправлять в справочник.
+            <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_DWH/master/file/stage.png' style='width: 70%; height: auto;'><br>
+            
+            
+            Таблицы слоя Core и схема переноса старых данных:
+            Таблицы и связи между ними изображены на схеме. Справа на схеме логика работы с 
+            устаревшими записями. Зеленым выделено то что реализовано в данный момент. В 
+            будущем планируется реализация третьего варианта работы с устаревшими записями.
+            Все процедуры по переносу данных присутствуют в папке func_core.
+            Запускаются они в ETL следующим кодом:
+            ```
+            def update_core():
+                # Обновление столбца статуса
+                sqlt_stmt = "select core.add_status();"
+                execute_stmt(sqlt_stmt)
+            
+                # Обновление справочников
+                sqlt_stmt = "select core.update_core_ref();"
+                execute_stmt(sqlt_stmt)
+            
+                # Добавление новых записей
+                sqlt_stmt = "SELECT core.add_fact_vacancy_0();"
+                execute_stmt(sqlt_stmt)
+            
+                # Перенос записей в историю и удаление перенесенных вакансий
+                sqlt_stmt = "SELECT core.update_fact_history();"
+                execute_stmt(sqlt_stmt)
+            
+                # Обновление записей
+                sqlt_stmt = "SELECT core.add_fact_vacancy_2();"
+                execute_stmt(sqlt_stmt)
+            
+                # Обновление таблицы мапинга скилы-вакансии
+                sqlt_stmt = "select core.add_skill_vacancy_map();"
+                execute_stmt(sqlt_stmt)
+            ```
+
+            <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_DWH/master/file/core.png' style='width: 100%; height: auto;'><br>
+            
+            Представление в слое Data Mart:
+            Этот слой нужен для того, чтобы подготовить данные для предоставления пользователям.
+            К нему же выдаются права доступа. Чтобы пользователь имел доступ только к тем данным к 
+            которым должен.
+            <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_DWH/master/file/data_mart.png' style='width: 70%; height: auto;'><br>
+            
+            ## Используемые технологии
+            
+            ![PosgreSQL](https://img.shields.io/badge/PosgreSQL-blue?logo=posgres)
+            ![SQL](https://img.shields.io/badge/SQL-yellow?logo=SQL)
+
+
+            """, unsafe_allow_html=True)
+
+    st.write("---")
+
+    with st.expander("Проект: Анализ вакансий на hh.ru. Report"):
+        st.markdown("""
+                ## Анализ вакансий на hh.ru
+
+                Данный репозиторий является частью проекта по анализу вакансий с hh.ru.  
+                Здесь будут размещены данные о ETL.
+                Всего таких репозиториев три:
+                1. pars_hh_ETL https://github.com/kulakov544/pars_hh_ETL.git
+                2. pars_hh_DWH https://github.com/kulakov544/pars_hh_DWH.git
+                3. pars_hh_Report https://github.com/kulakov544/pars_hh_report.git
+                
+                ### Бизнес требование
+                Нужно узнать какие навыки требуются на рынке вакансий.
+                
+                ### Техническое задание
+                Получить аналитику по требованиям к соискателям вакансий на сайте hh.ru по IT направлению.
+                
+                ### Решение
+                1. Построить базу DWH на PostgreSQL.
+                2. Получить данные о вакансиях с сайта hh.ru
+                3. Провести первичную обработка данных для загрузки в базу.
+                4. Загрузить данные в Stage слой DWH.
+                5. Трансформировать данные для укладки на долговременное хранение.
+                6. Заполнить справочники и таблицу вакансий в Core слое DWH, с сохранением обновленных записей в таблицу истории.
+                7. Оркестрация процесса через Prefect.
+                8. Сделать View и материализацию таблиц в DataMart слое DWH
+                9. По данным из DataMart построить дашборды в Yandex Data Lens и Power BI  
+
+                <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_report/master/file/DWH.png' style='width: 100%; height: auto;'><br>
+                                
+                ### Power BI
+                Дашборд призван ответить какие навыки и по каким типам вакансий требуются чаще всего.
+                Дополнительно добавлены счетчики для понимания количества вакансий выборке. Чем больше 
+                вакансий тем точнее данные.
+                <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_report/master/file/report_1.png' style='width: 100%; height: auto;'><br>
+                  
+                На второй странице размещена карта с местом нахождения вакансий. Она должна была показать в 
+                каких городах больше скопление вакансий. А за одно могла позволить выбирать вакансии 
+                по местонахождению и получать о них информацию. К сожалению вакансий с заполненными данными
+                о местонахождении крайне мало.
+                <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_report/master/file/report_2.png' style='width: 100%; height: auto;'><br>
+                В идеале должна была получиться похожая картина во многих городах и скопления точек разной 
+                плотности на карте страны.
+                <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_report/master/file/report_3.png' style='width: 100%; height: auto;'><br>
+                На третьей странице можно посмотреть топ компаний по количеству вакансий с различными 
+                параметрами.
+                <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_report/master/file/report_4.png' style='width: 100%; height: auto;'><br>
+                Четвертая страница сделана для поиска вакансий по некоторым параметрам. После чего их можно 
+                отсортировать в одном списке.
+                <img src='https://raw.githubusercontent.com/kulakov544/pars_hh_report/master/file/report_5.png' style='width: 100%; height: auto;'><br>
+                  
+                Файл отчета для PowerBI есть в репозитории. 
+                """, unsafe_allow_html=True)
--- page/show_skills.py
+++ page/show_skills.py
@@ -11,42 +11,68 @@ def show_skills():
     st.markdown("""
             ### Программирование и библиотеки Python ###
             - <img src="https://img.icons8.com/color/48/000000/python.png" width="24"/> Python
+                - Язык программирования на котором я пишу все свои проекты.
             - <img src="https://img.icons8.com/color/48/000000/git.png" width="24"/> GIT
+                - Система контроля версий. Позволяет создавать точки сохранения кода и возвращатся к ним. 
+                Работать командой над одним кодом, создавая ветки и сливая их с основным кодом.
             - <img src="https://cdn.hackersandslackers.com/2020/08/sqlalchemy2.png" width="24"/> SQLAlchemy
+                - Библиотека для работы с базой данных.
             - <img src="https://img.icons8.com/color/48/000000/pandas.png" width="24"/> Pandas
+                - Библиотека для обработки и анализа данных.
             - <i class="fas fa-robot"></i> telebot
+                - Библиотека для создания телеграмм-ботов.
             - <img src="https://img.icons8.com/color/48/000000/python.png" width="24"/> Requests
+                - Библиотека позволяющая работать со всеми видами запросов к серверу.
             - <img src="https://global.discourse-cdn.com/business7/uploads/prefecthq/original/2X/4/4551612629fb3d641edb1263395b39fac2ef04ca.svg" width="24"/> Prefect
+                - Оркестратор. Позволяет автоматизировать запуск программ и контроль работы.
 
             ### Работа с базами данных ###
-            - <img src="https://img.icons8.com/color/48/000000/sql.png" width="24"/> SQL       
+            - <img src="https://cdn-icons-png.flaticon.com/512/4299/4299956.png" width="24"/> SQL 
+                - Язык для написания запросов к базе данных.      
             - <img src="https://cdn.iconscout.com/icon/free/png-256/free-postgresql-8-1175119.png?f=webp" width="24"/> PostgreSQL
+                - База данных которой я пользуюсь.
             - <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/DBeaver_logo.svg/1200px-DBeaver_logo.svg.png" width="24"/> DBeaver
+                - Удобное приложение для работы с базой данных.
             - <img src="https://odbcmanager.net/images/icon256.gif" width="24"/> ODBC
+                - Драйвер для создания подключения к базе данных в Windows.
 
             ### Деплой ###
             - <img src="https://img.icons8.com/color/48/000000/docker.png" width="24"/> Docker
+                - Программа для деплоя приложений с контейнеризацией.
             - <img src="https://img.icons8.com/color/48/000000/filezilla.png" width="24"/> FileZilla
+                - Программа для FTP подключения к серверу.
             - <img src="https://research.reading.ac.uk/act/wp-content/uploads/sites/2/Unorganized/ssh.png" width="24"/> SSH
+                - Подключение к командной строке удаленного сервера.
 
             ### Веб-серверы и сети ###
             - <img src="https://img.icons8.com/color/48/000000/nginx.png" width="24"/> Nginx
+                - Прокси-сервер позволяющий настроить доступ к приложениям и страницам на сервере по доменным именам.
             - <img src="https://img.icons8.com/color/48/000000/shield.png" width="24"/> WireGuard
+                - Протокол на котором построена моя домашняя VPN сеть.
             - <img src="https://img.icons8.com/color/48/000000/remote-desktop.png" width="24"/> RDP
+                - Подключение к удаленному рабочему столу Windows. В моем случае это домашний сервер.
 
             ### Инструменты для разработки и тестирования ###
             - <img src="https://img.icons8.com/color/48/000000/pycharm.png" width="24"/> Pycharm
+                - Мощьное IDE которым я пользуюсь.
             - <img src="https://cdn.iconscout.com/icon/free/png-256/free-postman-3521648-2945092.png?f=webp&w=256" width="24"/> Postman
+                - Программа для тестирования API. Так же удобно когда нужно делать одиночные запросы и ответы.
             - <img src="https://static-00.iconduck.com/assets.00/window-dev-tools-icon-2048x1903-h6vcfy5j.png" width="24"/> DevTools
+                - Инструмент для работы с кодами веб-страниц. 
             - <img src="https://static-00.iconduck.com/assets.00/file-type-drawio-icon-2048x2048-dxjfklgq.png" width="24"/> Draw.io
+                - Приложение для рисования схем.
 
             ### Визуализация данных и отчеты ###
-            - <img src="https://img.icons8.com/color/48/000000/power-bi.png" width="24"/> Power BI
+            - <img src="https://1000logos.net/wp-content/uploads/2022/08/Microsoft-Power-BI-Logo.png" width="24"/> Power BI
             - <img src="https://336118.selcdn.ru/Gutsy-Culebra/products/Yandex-DataLens-Logo.png" width="24"/> Yandex DataLens
 
             ### Другое ###
             - <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Logo_Yandex.Tracker_2018.svg/1200px-Logo_Yandex.Tracker_2018.svg.png" width="24"/> Yandex Tracker
+                - Сервис для управления проектами и контроля выполнения задач. Активно пользуюсь Канбан доской данного сервиса.
             - <img src="https://img.icons8.com/color/48/000000/ubuntu.png" width="24"/> Ubuntu
+                - Система достаточно часто встречающаяся на серверах как и другие linux системы. Как правило для настройки 
+                сервера требуется знание командной строки этой операционной системы.
             - <img src="https://img.icons8.com/color/48/000000/windows-10.png" width="24"/> Windows 10
+                - Операционная система на которой я работаю.
             """, unsafe_allow_html=True)
 
